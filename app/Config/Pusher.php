839d3ea4c98936cd674b3f8046f10a4a3c195fe1
Roberto Alejandro Vásquez Alcántara <100809900+robertojonh@users.noreply.github.com>
2025-01-16 13:19:22 -0600

    Update Pusher.php

--- app/Config/Pusher.php
+++ app/Config/Pusher.php
@@ -6,9 +6,9 @@ use CodeIgniter\Config\BaseConfig;
 
 class Pusher extends BaseConfig
 {
-    public $app_id = '1823346';
-    public $key = 'a88403b3e52c3753b043';
-    public $secret = '2c6c8754640fffa6b9ad';
-    public $cluster = 'us2';
+    public $app_id = '';
+    public $key = '';
+    public $secret = '';
+    public $cluster = '';
     public $useTLS = true;
 }
